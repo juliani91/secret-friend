ccfd4a04b4452c3c6b964c25751cca3214612d91
Julian <Julian@Loeracbi.com>
2016-05-19 12:47:02 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Raffle</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,100italic,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <header class="">
+	  <div class="container">
+	  		<div class="eight columns offset-by-two">
+	  			<div class="logo">
+	  				<a href="#"> <img src="http://placehold.it/88x88"> </a>
+	  			</div>
+	  		</div>
+	  	</div>
+	  	<div class="container">
+	  		<div class="eight columns offset-by-two headertext">
+	  			<h1>Secret Friend Generator</h1>
+	  			<p>Just Use &nbsp; <del>Paper</del> &nbsp; E-mails</p>
+	  		</div>
+	  		   <div class="row">
+			      <div class="six columns alpha header-btn left-btn">
+			          <a href="#">Get Started</a>
+			      </div>
+			        <div class="six columns omega header-btn right-btn">
+			          <a href="#">Github</a>
+			      	</div>
+			    </div>
+	  	</div>
+  </header>
+
+  <main role="main">
+
+  </main>
+  <footer>
+
+  </footer>
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+  <script type="text/javascript" src="js/functions.js"></script>
+</body>
+</html>
